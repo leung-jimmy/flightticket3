2cf84bee46324f3f3603286db1c36f18bf70daa8
Jingmin Liang <leungjimmy82218@hotmail.com>
2025-07-05 19:34:02 +0800

    Initial commit of the flightticket3

---
+++ src/main/java/com/jimmy/flightticket3/Flightticket3Application.java
@@ -0,0 +1,13 @@
+package com.jimmy.flightticket3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flightticket3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Flightticket3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jimmy/flightticket3/Flightticket3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimmy.flightticket3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Flightticket3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
